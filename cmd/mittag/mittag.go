a04bb318c96916ee8a772a88083a3bead76088c3
Florian Hoss <mail@florianhoss.de>
2024-06-13 20:34:30 +0200

    Move things around

--- cmd/mittag/mittag.go
+++ cmd/mittag/mittag.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"log/slog"
-	"net/http"
 	"os"
 
-	"gitlab.unjx.de/flohoss/mittag/internal/controller"
+	"gitlab.unjx.de/flohoss/mittag/internal/config"
 	"gitlab.unjx.de/flohoss/mittag/internal/env"
 	"gitlab.unjx.de/flohoss/mittag/internal/logger"
-	"gitlab.unjx.de/flohoss/mittag/internal/router"
 )
 
 func main() {
@@ -20,13 +18,6 @@ func main() {
 	}
 	slog.SetDefault(logger.NewLogger(env.LogLevel))
 
-	r := router.InitRouter()
-	c := controller.NewController(env)
-	router.SetupRoutes(r, c, env.AdminKey)
-
-	slog.Info("starting server", "url", fmt.Sprintf("http://localhost:%d", env.Port), "version", env.Version)
-	if err := r.Start(fmt.Sprintf(":%d", env.Port)); err != http.ErrServerClosed {
-		slog.Error("cannot start server", "err", err)
-		os.Exit(1)
-	}
+	config := config.NewConfig()
+	fmt.Println(config)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"encoding/json"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+const ConfigLocation = "internal/config/restaurants/"
+
+type Config struct {
+	Restaurants map[string]*Restaurant
+}
+
+func NewConfig() *Config {
+	c := &Config{
+		Restaurants: parseConfigFiles(),
+	}
+	return c
+}
+
+func parseConfigFile(path string) (Restaurant, error) {
+	slog.Debug("parsing config file", "path", path)
+	var restaurant Restaurant
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return restaurant, err
+	}
+	err = json.Unmarshal(content, &restaurant)
+	if err != nil {
+		return restaurant, err
+	}
+
+	slog.Debug("config successfully parsed", "path", path)
+	return restaurant, nil
+}
+
+func parseConfigFiles() map[string]*Restaurant {
+	restaurants := make(map[string]*Restaurant)
+
+	filepath.WalkDir(ConfigLocation, func(path string, info os.DirEntry, err error) error {
+		if info.Type().IsRegular() {
+			config, err := parseConfigFile(path)
+			if err != nil {
+				slog.Debug("error parsing config file", "path", path, "err", err)
+				return filepath.SkipDir
+			}
+			restaurants[config.ID] = &config
+		}
+		return nil
+	})
+
+	return restaurants
+}
