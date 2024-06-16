d6c8a17c30eff4a4675cb81a161ec21999bf301e
Florian Hoss <mail@florianhoss.de>
2024-06-16 16:38:34 +0200

    Use groups in pinia as well

--- web/src/stores/restaurants.ts
+++ web/src/stores/restaurants.ts
@@ -4,11 +4,28 @@ import { RestaurantsService, handler_Restaurant } from 'src/openapi';
 export const useRestaurantStore = defineStore('restaurant', {
   state: () => ({
     restaurant: {} as handler_Restaurant,
+    restaurants: {} as Record<string, handler_Restaurant>,
   }),
+  getters: {
+    grouped() {
+      const groupMap: Record<string, handler_Restaurant[]> = {};
+      for (const [key, value] of Object.entries(this.restaurants)) {
+        if (!groupMap[key]) {
+          groupMap[key] = [];
+        }
+        groupMap[key].push(value);
+      }
+      return groupMap;
+    },
+  },
   actions: {
     async getRestaurant(name: string) {
       const response = await RestaurantsService.getRestaurants1(name);
       this.$state.restaurant = response;
+    },
+    async getRestaurants() {
+      const response = await RestaurantsService.getRestaurants();
+      this.$state.restaurants = response;
     },
   },
 });
