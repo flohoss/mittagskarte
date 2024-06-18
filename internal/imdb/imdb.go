38ebe831e1fde799ec6cd728a188159ea9993425
Florian Hoss <mail@florianhoss.de>
2024-06-18 06:52:11 +0200

    Move imdb logic to restaurant

--- internal/imdb/imdb.go
+++ internal/imdb/imdb.go
@@ -2,12 +2,9 @@ package imdb
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log/slog"
 
 	"github.com/redis/go-redis/v9"
-	"gitlab.unjx.de/flohoss/mittag/internal/config"
 )
 
 // in-memory database (IMDb
@@ -26,33 +23,4 @@ func New(host string, port int) *IMDb {
 		Redis: rdb,
 		CTX:   context.Background(),
 	}
-}
-
-func (imdb *IMDb) SaveMenu(restaurant *config.Restaurant) {
-	value, err := json.Marshal(restaurant.Menu)
-	if err != nil {
-		slog.Error("could not marshal menu", "restaurant", restaurant.Name, "err", err)
-		return
-	}
-	key := restaurant.ID + "-menu"
-	menu := string(value)
-	slog.Debug("saving menu", "key", key, "value", menu)
-	err = imdb.Redis.Set(imdb.CTX, key, value, 0).Err()
-	if err != nil {
-		slog.Error("could not save menu", "key", key, "value", menu, "err", err)
-	}
-}
-
-func (imdb *IMDb) RestoreMenu(restaurant *config.Restaurant) {
-	key := restaurant.ID + "-menu"
-	value, err := imdb.Redis.Get(imdb.CTX, key).Result()
-	if err != nil {
-		slog.Warn("could not restore menu", "restaurant", restaurant.Name, "err", err)
-		return
-	}
-	err = json.Unmarshal([]byte(value), &restaurant.Menu)
-	if err != nil {
-		slog.Error("could not unmarshal menu", "restaurant", restaurant.Name, "err", err)
-	}
-	slog.Debug("menu restored", "key", key, "value", value)
 }
