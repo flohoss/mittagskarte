43d5a072c39f175d004ca6d22974da6344cb5d71
Florian Hoss <mail@florianhoss.de>
2024-06-23 20:26:11 +0200

    Add countdown

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -1,7 +1,103 @@
 <script setup lang="ts">
+import moment from 'moment';
+import { LocalStorage } from 'quasar';
+import { computed, onMounted, reactive, ref } from 'vue';
+
+const countdown = reactive({ hour: 0, minute: 0, second: 0 });
+const countdownStatus = reactive({ almostDone: false, over: false });
+const selectedTime = ref(
+  (LocalStorage.getItem('mittag_time') as string) || '1300'
+);
+
+const midday = computed(() => {
+  const hour = parseInt(selectedTime.value.substring(0, 2));
+  const minute = parseInt(selectedTime.value.substring(2, 4));
+  return moment().hours(hour).minute(minute).second(0);
+});
+
+function update() {
+  const diff = midday.value.diff(moment());
+  if (diff <= 0) {
+    countdownStatus.over = true;
+    countdown.hour = 0;
+    countdown.minute = 0;
+    countdown.second = 0;
+  } else {
+    countdownStatus.over && (countdownStatus.over = false);
+    countdownStatus.almostDone = diff > 0 && diff <= 3600000 ? true : false;
+    countdown.hour = Math.floor((diff % 86400000) / 3600000);
+    countdown.minute = Math.floor((diff % 3600000) / 60000);
+    countdown.second = Math.floor((diff % 60000) / 1000);
+  }
+}
+
+onMounted(() => {
+  update();
+  setInterval(() => update(), 1000);
+});
+
+const timerClass = computed(() => {
+  if (countdownStatus.almostDone && !countdownStatus.over)
+    return 'text-warning';
+  else if (countdownStatus.over) return 'text-negative';
+  else return '';
+});
 </script>
 
 <template>
-  <q-page class="column justify-stard items-center q-pt-md q-gutter-lg">
+  <q-page
+    class="row justify-center items-center q-gutter-md"
+    :class="timerClass"
+  >
+    <div class="column items-center">
+      <span class="countdown" :class="$q.screen.lt.md ? 'text-h2' : 'text-h1'">
+        <span :style="{ '--value': countdown.hour }"></span>
+      </span>
+      <span class="text-subtitle">Stunden</span>
+    </div>
+    <div class="column items-center">
+      <span class="countdown" :class="$q.screen.lt.md ? 'text-h2' : 'text-h1'">
+        <span :style="{ '--value': countdown.minute }"></span>
+      </span>
+      <span class="text-subtitle">Minuten</span>
+    </div>
+    <div class="column items-center">
+      <span class="countdown" :class="$q.screen.lt.md ? 'text-h2' : 'text-h1'">
+        <span :style="{ '--value': countdown.second }"></span>
+      </span>
+      <span class="text-subtitle">Sekunden</span>
+    </div>
   </q-page>
 </template>
+
+<style scoped>
+.text-subtitle {
+  transition: all 1s cubic-bezier(1, 0, 0, 1);
+  font-family: monospace;
+  font-size: large;
+}
+
+:root .countdown {
+  line-height: 1em;
+  font-family: monospace;
+}
+
+.countdown {
+  display: inline-flex;
+}
+
+.countdown > * {
+  height: 1em;
+  display: inline-block;
+  overflow-y: hidden;
+}
+
+.countdown > *:before {
+  position: relative;
+  content: '00\A 01\A 02\A 03\A 04\A 05\A 06\A 07\A 08\A 09\A 10\A 11\A 12\A 13\A 14\A 15\A 16\A 17\A 18\A 19\A 20\A 21\A 22\A 23\A 24\A 25\A 26\A 27\A 28\A 29\A 30\A 31\A 32\A 33\A 34\A 35\A 36\A 37\A 38\A 39\A 40\A 41\A 42\A 43\A 44\A 45\A 46\A 47\A 48\A 49\A 50\A 51\A 52\A 53\A 54\A 55\A 56\A 57\A 58\A 59\A 60\A 61\A 62\A 63\A 64\A 65\A 66\A 67\A 68\A 69\A 70\A 71\A 72\A 73\A 74\A 75\A 76\A 77\A 78\A 79\A 80\A 81\A 82\A 83\A 84\A 85\A 86\A 87\A 88\A 89\A 90\A 91\A 92\A 93\A 94\A 95\A 96\A 97\A 98\A 99\A';
+  white-space: pre;
+  top: calc(var(--value) * -1em);
+  text-align: center;
+  transition: all 1s cubic-bezier(1, 0, 0, 1);
+}
+</style>
