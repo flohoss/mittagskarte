76b9a68a216859334601b401b31e048fab54b6ea
Florian Hoss <mail@florianhoss.de>
2024-06-23 06:33:58 +0200

    Fix restaurant header

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -30,26 +30,20 @@ const toggleLeftDrawer = () => {
     </q-drawer>
 
     <q-page-container>
-      <q-btn
-        v-if="!Screen.gt.sm"
-        fab
-        round
-        color="primary"
-        icon="fa-solid fa-bars"
-        @click="toggleLeftDrawer"
-        :class="{ 'text-white': Dark.isActive, 'text-black': !Dark.isActive }"
-        class="drawer-btn"
-      />
       <router-view />
     </q-page-container>
   </q-layout>
-</template>
 
-<style scoped>
-.drawer-btn {
-  z-index: 1;
-  position: absolute;
-  bottom: 1rem;
-  right: 1rem;
-}
-</style>
+  <Teleport to="body">
+    <q-btn
+      v-if="Screen.lt.md"
+      fab
+      round
+      color="primary"
+      icon="fa-solid fa-bars"
+      @click="toggleLeftDrawer"
+      :class="{ 'text-white': Dark.isActive, 'text-black': !Dark.isActive }"
+      style="z-index: 1; position: fixed; bottom: 1rem; right: 1rem"
+    />
+  </Teleport>
+</template>
