7396b1f6d36c65a8c4dc962701a8b59891668200
Florian Hoss <mail@florianhoss.de>
2024-06-23 05:13:21 +0200

    Remove top bar

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
 import MainNavigation from 'components/MainNavigation.vue';
 import { ref } from 'vue';
 import { Dark } from 'quasar';
+import { Screen } from 'quasar';
 
 const leftDrawerOpen = ref(false);
 
@@ -11,18 +12,7 @@ const toggleLeftDrawer = () => {
 </script>
 
 <template>
-  <q-layout view="lHh LpR lFf">
-    <q-header
-      class="bg-transparent"
-      :class="{ 'text-white': Dark.isActive, 'text-black': !Dark.isActive }"
-    >
-      <q-toolbar>
-        <q-btn flat round icon="fa-solid fa-bars" @click="toggleLeftDrawer" />
-
-        <q-space />
-      </q-toolbar>
-    </q-header>
-
+  <q-layout view="hHh LpR fFf">
     <q-drawer
       show-if-above
       v-model="leftDrawerOpen"
@@ -40,7 +30,26 @@ const toggleLeftDrawer = () => {
     </q-drawer>
 
     <q-page-container>
+      <q-btn
+        v-if="!Screen.gt.sm"
+        fab
+        round
+        color="primary"
+        icon="fa-solid fa-bars"
+        @click="toggleLeftDrawer"
+        :class="{ 'text-white': Dark.isActive, 'text-black': !Dark.isActive }"
+        class="drawer-btn"
+      />
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
+
+<style scoped>
+.drawer-btn {
+  z-index: 1;
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+}
+</style>
