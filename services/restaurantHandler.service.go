f85737a248961cff01ee9166bf51ee935994579f
Florian Hoss <mail@florianhoss.de>
2024-09-18 06:16:41 +0200

    Change to rod

--- services/restaurantHandler.service.go
+++ services/restaurantHandler.service.go
@@ -2,16 +2,14 @@ package services
 
 import (
 	"errors"
-	"fmt"
 	"log/slog"
 	"os"
-
-	"gitlab.unjx.de/flohoss/mittag/pgk/fetch"
 )
 
 const (
 	storageLocation  = "storage/"
 	downloadLocation = storageLocation + "downloads/"
+	rawLocation      = downloadLocation + "raw/"
 )
 
 func init() {
@@ -43,6 +41,7 @@ func (r *RestaurantHandler) getImageUrls() {
 			continue
 		}
 
+		rawPath := rawLocation + id
 		filePath := downloadLocation + id + ".webp"
 		if _, err := os.Stat(filePath); !os.IsNotExist(err) {
 			r.restaurants[id].ImageUrl = filePath
@@ -50,22 +49,31 @@ func (r *RestaurantHandler) getImageUrls() {
 			continue
 		}
 
-		cdp := NewChromeDp()
-		if err := cdp.NavigateToFinalUrl(r.restaurants[id].PageUrl, &r.restaurants[id].FinalUrl, r.restaurants[id].Parse); err != nil {
-			slog.Error("cannot get final url", "err", err)
-			cdp.Close()
-			continue
-		}
-
+		cdp := NewScraper()
 		if r.restaurants[id].Parse.IsFile {
-			if err := r.handleFile(filePath, id); err != nil {
+			if err := cdp.DownloadFile(r.restaurants[id].PageUrl, rawPath, r.restaurants[id].Parse); err != nil {
 				slog.Error("cannot handle file", "id", id, "err", err)
 				cdp.Close()
 				continue
 			}
+			if err := r.im.ConvertToWebp(rawPath, filePath); err != nil {
+				slog.Error("cannot convert to webp", "id", id, "err", err)
+				cdp.Close()
+				continue
+			}
 		} else {
-			if err := r.handleScreenshot(filePath, id, cdp); err != nil {
-				slog.Error("cannot handle screenshot", "id", id, "err", err)
+			if err := cdp.Screenshot(r.restaurants[id].PageUrl, rawPath, r.restaurants[id].Parse); err != nil {
+				slog.Error("cannot take screenshot", "id", id, "err", err)
+				cdp.Close()
+				continue
+			}
+			if err := r.im.ConvertToWebp(rawPath, filePath); err != nil {
+				slog.Error("cannot convert to webp", "id", id, "err", err)
+				cdp.Close()
+				continue
+			}
+			if err := r.im.Crop(filePath, r.restaurants[id].Parse.Scan.Crop); err != nil {
+				slog.Error("cannot crop image", "id", id, "err", err)
 				cdp.Close()
 				continue
 			}
@@ -73,29 +81,6 @@ func (r *RestaurantHandler) getImageUrls() {
 		cdp.Close()
 		r.restaurants[id].ImageUrl = filePath
 	}
-}
-
-func (r *RestaurantHandler) handleFile(filePath string, id string) error {
-	downloadPath, err := fetch.DownloadFile(id, r.restaurants[id].FinalUrl)
-	if err != nil {
-		return fmt.Errorf("cannot download from '%s': %w", r.restaurants[id].FinalUrl, err)
-	}
-	if err := r.im.ConvertToWebp(downloadPath, filePath); err != nil {
-		return fmt.Errorf("cannot convert to webp: %w", err)
-	}
-	slog.Info("file downloaded", "filePath", filePath)
-	return nil
-}
-
-func (r *RestaurantHandler) handleScreenshot(filePath string, id string, cdp *ChromeDp) error {
-	if err := cdp.Screenshot(r.restaurants[id].FinalUrl, filePath, r.restaurants[id].Parse.Scan.Chrome, r.restaurants[id].Parse); err != nil {
-		return fmt.Errorf("cannot make screenshot: %w", err)
-	}
-	if err := r.im.Crop(filePath, r.restaurants[id].Parse.Scan.Crop); err != nil {
-		return fmt.Errorf("cannot crop screenshot: %w", err)
-	}
-	slog.Info("screenshot saved", "filePath", filePath)
-	return nil
 }
 
 func (r *RestaurantHandler) GetAllRestaurants() map[string]*Restaurant {
