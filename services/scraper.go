ce4ecc5453208bfadbb76703ba2f6d7ca9b3d594
Florian Hoss <mail@florianhoss.de>
2024-09-26 13:38:38 +0200

    Write some functions

--- services/scraper.go
+++ services/scraper.go
@@ -26,6 +26,8 @@ const (
 
 type Scraper struct {
 	browser *rod.Browser
+	page    *rod.Page
+	info    *proto.TargetTargetInfo
 }
 
 func NewScraper() *Scraper {
@@ -49,34 +51,32 @@ func (cdp *Scraper) Close() {
 	}
 }
 
-func (cdp *Scraper) NavigateToFinalUrl(parse Parse) {
-	if len(parse.Navigate) == 0 {
-		return
-	}
-}
-
-func selectTheRightMethod(page *rod.Page, selector Selector) *rod.Element {
+func (cdp *Scraper) selectTheRightMethod(selector *Selector) (*rod.Element, error) {
 	switch selector.SearchBy {
 	case CSS:
 		slog.Debug("searching by css-selector", "search", selector.Search)
-		return page.MustElement(selector.Search)
+		return cdp.page.Element(selector.Search)
 	case XPath:
 		slog.Debug("searching by xpath-selector", "search", selector.Search)
-		return page.MustElementX(selector.Search)
+		return cdp.page.ElementX(selector.Search)
 	case Name:
 		slog.Debug("searching by name-selector", "search", selector.Search)
-		return page.MustSearch(selector.Search)
+		res, err := cdp.page.Search(selector.Search)
+		if err != nil {
+			return nil, err
+		}
+		return res.First, nil
 	case Regex:
 		slog.Debug("searching by regex-selector", "regex", selector.Regex)
-		return page.MustElementR(selector.Search, selector.Regex)
+		return cdp.page.ElementR(selector.Search, selector.Regex)
 	default:
 		slog.Debug("no valid selector")
 		os.Exit(1)
-		return nil
 	}
+	return nil, nil
 }
 
-func (cdp *Scraper) Screenshot(url string, filePath string, parse Parse) error {
+func (cdp *Scraper) navigateToFirstPage(url string) error {
 	opts := proto.TargetCreateTarget{
 		URL: url,
 	}
@@ -84,28 +84,52 @@ func (cdp *Scraper) Screenshot(url string, filePath string, parse Parse) error {
 	if err != nil {
 		return fmt.Errorf("failed to get page: %w", err)
 	}
+	cdp.page = page.Timeout(1 * time.Minute)
 
-	if err := page.Timeout(1 * time.Minute).WaitStable(5 * time.Second); err != nil {
+	if err := cdp.page.WaitStable(5 * time.Second); err != nil {
 		return fmt.Errorf("failed to wait for page stability: %w", err)
 	}
 
-	info, err := page.Info()
+	info, err := cdp.page.Info()
 	if err != nil {
 		return fmt.Errorf("failed to get page info: %w", err)
 	}
+	cdp.info = info
+	return nil
+}
 
+func (cdp *Scraper) navigateToAction(n *Selector) error {
+	slog.Debug("navigating", "url", cdp.info.URL)
+	el, err := cdp.selectTheRightMethod(n)
+	if err != nil {
+		return fmt.Errorf("failed to get element: %w", err)
+	}
+	if err := el.Click(proto.InputMouseButtonLeft, 1); err != nil {
+		return fmt.Errorf("failed to click element: %w", err)
+	}
+	if err := cdp.page.WaitStable(5 * time.Second); err != nil {
+		return fmt.Errorf("failed to wait for page stability after click: %w", err)
+	}
+	return nil
+}
+
+func (cdp *Scraper) screenshot(url string, filePath string, parse Parse) error {
 	for _, n := range parse.Navigate {
-		slog.Debug("navigating", "url", info.URL)
-		if err := selectTheRightMethod(page, n).Click(proto.InputMouseButtonLeft, 1); err != nil {
-			return fmt.Errorf("failed to click element: %w", err)
-		}
-		if err := page.WaitStable(5 * time.Second); err != nil {
-			return fmt.Errorf("failed to wait for page stability after click: %w", err)
+		if err := cdp.navigateToAction(&n); err != nil {
+			return fmt.Errorf("failed to navigate to action: %w", err)
 		}
 	}
 
-	slog.Debug("making screenshot", "url", info.URL)
-	img, err := page.MustSetViewport(parse.Scan.ViewportWidth, 0, 1, false).ScrollScreenshot(&rod.ScrollScreenshotOptions{
+	if err := cdp.page.SetViewport(&proto.EmulationSetDeviceMetricsOverride{
+		Height:      0,
+		Mobile:      false,
+		ScreenWidth: &parse.Scan.ViewportWidth,
+	}); err != nil {
+		return fmt.Errorf("failed to set viewport: %w", err)
+	}
+
+	slog.Debug("making screenshot", "url", cdp.info.URL)
+	img, err := cdp.page.ScrollScreenshot(&rod.ScrollScreenshotOptions{
 		Format:      proto.PageCaptureScreenshotFormatPng,
 		FixedTop:    parse.Scan.FixedTop,
 		FixedBottom: parse.Scan.FixedBottom,
@@ -121,29 +145,19 @@ func (cdp *Scraper) Screenshot(url string, filePath string, parse Parse) error {
 	return nil
 }
 
-func (cdp *Scraper) DownloadFile(url string, filePath string, parse Parse) error {
-	opts := proto.TargetCreateTarget{
-		URL: url,
-	}
-
-	page, err := cdp.browser.Page(opts)
-	if err != nil {
-		return fmt.Errorf("failed to get page: %w", err)
-	}
-
-	if err := page.Timeout(1 * time.Minute).WaitStable(5 * time.Second); err != nil {
-		return fmt.Errorf("failed to wait for page stability: %w", err)
-	}
-
-	info, err := page.Info()
-	if err != nil {
-		return fmt.Errorf("failed to get page info: %w", err)
-	}
-
+func (cdp *Scraper) downloadFile(url string, filePath string, parse Parse) error {
 	for _, n := range parse.Navigate {
-		if n.Attribute != "" {
-			slog.Debug("downloading file", "url", info.URL)
-			attrValue, err := selectTheRightMethod(page, n).Attribute(n.Attribute)
+		if n.Attribute == "" {
+			if err := cdp.navigateToAction(&n); err != nil {
+				return fmt.Errorf("failed to navigate to action: %w", err)
+			}
+		} else {
+			slog.Debug("downloading file", "url", cdp.info.URL)
+			el, err := cdp.selectTheRightMethod(&n)
+			if err != nil {
+				return fmt.Errorf("failed to get element: %w", err)
+			}
+			attrValue, err := el.Attribute(n.Attribute)
 			if err != nil {
 				return fmt.Errorf("failed to get attribute %s: %w", n.Attribute, err)
 			}
@@ -151,14 +165,6 @@ func (cdp *Scraper) DownloadFile(url string, filePath string, parse Parse) error
 			fullURL := fmt.Sprintf("%s%s", n.Prefix, *attrValue)
 			if err := fetch.DownloadFile(filePath, fullURL); err != nil {
 				return fmt.Errorf("failed to download file from URL %s: %w", fullURL, err)
-			}
-		} else {
-			slog.Debug("navigating", "url", info.URL)
-			if err := selectTheRightMethod(page, n).Click(proto.InputMouseButtonLeft, 1); err != nil {
-				return fmt.Errorf("failed to click element: %w", err)
-			}
-			if err := page.WaitStable(5 * time.Second); err != nil {
-				return fmt.Errorf("failed to wait for page stability after click: %w", err)
 			}
 		}
 	}
