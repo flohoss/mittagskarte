8b308ab2b179b61c2c97879ec631d7258e20dd79
Florian Hoss <mail@florianhoss.de>
2024-09-26 17:48:01 +0200

    Cleaner code

--- services/imageMagic.service.go
+++ services/imageMagic.service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"fmt"
+	"log/slog"
+
 	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
@@ -19,47 +22,50 @@ func (ic *ImageMagic) Close() {
 }
 
 func (ic *ImageMagic) Crop(filePath string, crop Crop) error {
+	slog.Debug("cropping image", "path", filePath)
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 	if err := mw.ReadImage(filePath); err != nil {
-		return err
+		return fmt.Errorf("failed to read image: %w", err)
 	}
 	if err := mw.CropImage(crop.Width, crop.Height, crop.OffsetX, crop.OffsetY); err != nil {
-		return err
+		return fmt.Errorf("failed to crop image: %w", err)
 	}
 	if err := mw.WriteImage(filePath); err != nil {
-		return err
+		return fmt.Errorf("failed to write image: %w", err)
 	}
 	return nil
 }
 
 func (ic *ImageMagic) ConvertToWebp(oldFilePath string, newFilePath string) error {
+	slog.Debug("converting image to webp", "path", oldFilePath)
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 	if err := mw.ReadImage(oldFilePath); err != nil {
-		return err
+		return fmt.Errorf("failed to read image: %w", err)
 	}
 	if err := mw.SetImageFormat("webp"); err != nil {
-		return err
+		return fmt.Errorf("failed to set image format webp: %w", err)
 	}
 	mw.SetCompressionQuality(100)
 	if err := mw.WriteImage(newFilePath); err != nil {
-		return err
+		return fmt.Errorf("failed to write image: %w", err)
 	}
 	return nil
 }
 
 func (ic *ImageMagic) Trim(filePath string) error {
+	slog.Debug("trimming image", "path", filePath)
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 	if err := mw.ReadImage(filePath); err != nil {
-		return err
+		return fmt.Errorf("failed to read image: %w", err)
 	}
 	if err := mw.TrimImage(0); err != nil {
-		return err
+		return fmt.Errorf("failed to trim image: %w", err)
 	}
 	if err := mw.WriteImage(filePath); err != nil {
-		return err
+		return fmt.Errorf("failed to write image: %w", err)
 	}
 	return nil
 }
--- services/mittag.service.go
+++ services/mittag.service.go
@@ -40,6 +40,35 @@ func (r *Mittag) Close() {
 	}
 }
 
+func (r *Mittag) handleRestaurant(s *ScraperService, id string, rawPath string, filePath string) {
+	defer s.Close()
+
+	s.navigateToFirstPage(r.restaurants[id].PageUrl)
+	if r.restaurants[id].Parse.IsFile {
+		if err := s.downloadFile(r.restaurants[id].PageUrl, rawPath, r.restaurants[id].Parse); err != nil {
+			slog.Error("cannot handle file", "id", id, "err", err)
+			return
+		}
+	} else {
+		if err := s.screenshot(r.restaurants[id].PageUrl, rawPath, r.restaurants[id].Parse); err != nil {
+			slog.Error("cannot take screenshot", "id", id, "err", err)
+			return
+		}
+		if err := r.im.Crop(rawPath, r.restaurants[id].Parse.Scan.Crop); err != nil {
+			slog.Error("cannot crop image", "id", id, "err", err)
+			return
+		}
+	}
+
+	if err := r.convertFinalWebp(id, rawPath, filePath); err != nil {
+		slog.Error("cannot convert file to final webp", "id", id, "err", err)
+		return
+	}
+	r.restaurants[id].ImageUrl = filePath
+	slog.Info("finished", "id", id, "filePath", filePath)
+	s.panicked <- false
+}
+
 func (r *Mittag) getImageUrls() {
 	for id := range r.restaurants {
 		if r.restaurants[id].PageUrl == "" {
@@ -49,46 +78,21 @@ func (r *Mittag) getImageUrls() {
 
 		rawPath := rawLocation + id
 		filePath := downloadLocation + id + ".webp"
-		if _, err := os.Stat(rawPath); os.IsNotExist(err) {
-			cdp := NewScraper()
-			err := cdp.navigateToFirstPage(r.restaurants[id].PageUrl)
-			if err != nil {
-				slog.Error("cannot navigate to first page", "id", id, "err", err)
-				cdp.Close()
-				continue
-			}
-			if r.restaurants[id].Parse.IsFile {
-				if err := cdp.downloadFile(r.restaurants[id].PageUrl, rawPath, r.restaurants[id].Parse); err != nil {
-					slog.Error("cannot handle file", "id", id, "err", err)
-					cdp.Close()
-					continue
-				}
-			} else {
-				if err := cdp.screenshot(r.restaurants[id].PageUrl, rawPath, r.restaurants[id].Parse); err != nil {
-					slog.Error("cannot take screenshot", "id", id, "err", err)
-					cdp.Close()
-					continue
-				}
-				cdp.Close()
-				if err := r.im.Crop(rawPath, r.restaurants[id].Parse.Scan.Crop); err != nil {
-					slog.Error("cannot crop image", "id", id, "err", err)
-					continue
-				}
-			}
-		}
-
-		if _, err := os.Stat(filePath); os.IsNotExist(err) {
-			if err := r.convertFinalWebp(id, rawPath, filePath); err != nil {
-				slog.Error("cannot convert file to final webp", "id", id, "err", err)
-				continue
-			}
-			r.restaurants[id].ImageUrl = filePath
-		} else {
-			r.restaurants[id].ImageUrl = filePath
+		if _, err := os.Stat(rawPath); !os.IsNotExist(err) {
 			slog.Debug("file already exists, skipping...", "filePath", filePath)
+			r.restaurants[id].ImageUrl = filePath
 			continue
+		} else {
+			s := NewScraperService(id)
+			go r.handleRestaurant(s, id, rawPath, filePath)
+
+			select {
+			case <-s.panicked:
+				continue
+			}
 		}
 	}
+	slog.Info("all done!")
 }
 
 func (r *Mittag) convertFinalWebp(id string, rawPath, filePath string) error {
