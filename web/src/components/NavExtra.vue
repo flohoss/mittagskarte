2629bec2c5f32184c587de516b043ee3fc8a7228
Florian Hoss <mail@florianhoss.de>
2024-10-10 20:14:06 +0200

    Fix settings button

--- web/src/components/NavExtra.vue
+++ web/src/components/NavExtra.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import RestaurantActions from './RestaurantActions.vue';
 import { services_CleanRestaurant } from 'src/openapi';
 import { useQuasar } from 'quasar';
 import RestaurantInfo from './RestaurantInfo.vue';
+import SettingsForm from './SettingsForm.vue';
 
 const $q = useQuasar();
 defineProps({
@@ -13,12 +14,14 @@ defineProps({
   },
 });
 
-const size = computed(() => {
-  if ($q.screen.lt.sm) {
+const iconSize = computed(() => {
+  if ($q.screen.lt.md) {
     return 'md';
   }
   return 'sm';
 });
+
+const settings = ref(false);
 </script>
 
 <template>
@@ -26,8 +29,21 @@ const size = computed(() => {
     <RestaurantInfo
       v-if="$q.screen.gt.sm && restaurant.image_url !== ''"
       :restaurant="restaurant"
-      :icon-size="size"
+      :icon-size="iconSize"
     />
-    <RestaurantActions :restaurant="restaurant" :icon-size="size" />
+    <RestaurantActions :restaurant="restaurant" :icon-size="iconSize" />
+    <q-btn
+      :size="iconSize"
+      round
+      flat
+      icon="fa-solid fa-gear"
+      @click="settings = true"
+    >
+      <q-tooltip>Einstellungen</q-tooltip>
+    </q-btn>
+
+    <q-dialog v-model="settings" backdrop-filter="blur(4px) saturate(150%)">
+      <SettingsForm />
+    </q-dialog>
   </div>
 </template>
