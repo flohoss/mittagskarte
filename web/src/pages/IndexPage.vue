d4abdd59a4cbb93ca7a0e43cc9b508543a719bff
Florian Hoss <mail@florianhoss.de>
2025-08-19 16:27:25 +0200

    Remove vue

--- web/src/pages/IndexPage.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import CountdownClock from 'src/components/CountdownClock.vue';
-</script>
-
-<template>
-    <q-page class="row items-center justify-center">
-        <CountdownClock />
-    </q-page>
-</template>
