16b1b54637137ab6cd9147cb9e64e5c3d9e74e3a
Florian Hoss <mail@florianhoss.de>
2025-11-05 19:05:16 +0100

    Ensure assets/js directory exists before copying files
    Adds a command to create the /app/assets/js directory if it does not exist, preventing errors when copying JavaScript dependencies during container startup.

--- docker/dev.entrypoint.sh
+++ docker/dev.entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+mkdir -p /app/assets/js
+
 cp /app/node_modules/htmx.org/dist/htmx.min.js /app/assets/js/htmx.min.js
 
 cp /app/node_modules/@floating-ui/core/dist/floating-ui.core.umd.min.js /app/assets/js/floating-ui.core.umd.min.js
