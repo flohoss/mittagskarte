f338c2717a3c009ab1a9a320a452692275575489
Florian Hoss <mail@florianhoss.de>
2026-01-13 11:48:27 +0100

    Integrate Sentry error tracking with Echo
    Added Sentry initialization and middleware to enable error tracking and reporting in the Echo web server. Also added request logging middleware for improved observability.

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/flohoss/mittagskarte/config"
 	"github.com/flohoss/mittagskarte/handlers"
 	"github.com/flohoss/mittagskarte/services"
+	"github.com/getsentry/sentry-go"
+	sentryecho "github.com/getsentry/sentry-go/echo"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -18,14 +20,29 @@ func setupRouter() *echo.Echo {
 	e.HideBanner = true
 	e.HidePort = true
 
+	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
 	e.Use(middleware.Gzip())
 
+	e.Use(sentryecho.New(sentryecho.Options{
+		Repanic: true,
+	}))
+
 	return e
 }
 
+func initSentry() {
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://773c98ff883e064b35dcd522404bac0d@o4510702473641984.ingest.de.sentry.io/4510702475411536",
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
+}
+
 func main() {
+	initSentry()
+
 	e := setupRouter()
 	config.New()
 
