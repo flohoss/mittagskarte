b6c3fa0609e7613089fde3cf05f237c6cad957fd
Florian Hoss <mail@florianhoss.de>
2026-03-26 20:43:35 +0100

    Use Pocketbase for backend

---
+++ backend/internal/mittag/restaurant.go
@@ -0,0 +1,194 @@
+package mittag
+
+import (
+	"fmt"
+	"log/slog"
+	"math/rand"
+	"net/url"
+	"path/filepath"
+	"time"
+
+	"github.com/flohoss/mittagskarte/internal/checksum"
+	"github.com/flohoss/mittagskarte/internal/download"
+	"github.com/flohoss/mittagskarte/internal/placeholder"
+	"github.com/flohoss/mittagskarte/internal/web"
+
+	"github.com/playwright-community/playwright-go"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+type Restaurant struct {
+	ID          string     `db:"id" json:"id"`
+	Website     string     `db:"website" json:"website"`
+	RestDays    []string   `db:"rest_days" json:"rest_days"`
+	Method      string     `db:"method" json:"method"`
+	ContentType string     `db:"content_type" json:"content_type"`
+	Cron        string     `db:"cron" json:"cron"`
+	Navigate    []Selector `db:"navigate" json:"navigate"`
+}
+
+type Selector struct {
+	Id        string `db:"id" json:"id"`
+	Locator   string `db:"locator" json:"locator"`
+	Attribute string `db:"attribute" json:"attribute"`
+	Style     string `db:"style" json:"style"`
+}
+
+func fetchRestaurants(app core.App) ([]*Restaurant, error) {
+	r, err := app.FindAllRecords("restaurants")
+	if err != nil {
+		return nil, err
+	}
+	app.ExpandRecords(r, []string{"navigate"}, nil)
+
+	restaurants := make([]*Restaurant, len(r))
+	for i, restaurant := range r {
+		expandedNavigate := restaurant.ExpandedAll("navigate")
+		navigate := make([]Selector, 0, len(expandedNavigate))
+		for _, nav := range expandedNavigate {
+			navigate = append(navigate, Selector{
+				Id:        nav.Id,
+				Locator:   nav.GetString("locator"),
+				Attribute: nav.GetString("attribute"),
+				Style:     nav.GetString("style"),
+			})
+		}
+		restaurants[i] = &Restaurant{
+			ID:          restaurant.Id,
+			Website:     restaurant.GetString("website"),
+			RestDays:    restaurant.GetStringSlice("rest_days"),
+			Method:      restaurant.GetString("method"),
+			ContentType: restaurant.GetString("content_type"),
+			Cron:        restaurant.GetString("cron"),
+			Navigate:    navigate,
+		}
+	}
+
+	return restaurants, nil
+}
+
+func (r *Restaurant) updateMenu(filePath string, app core.App) error {
+	restaurant, err := app.FindRecordById("restaurants", r.ID)
+	if err != nil {
+		return err
+	}
+
+	filePathWithChecksum, err := checksum.SuffixQuery(filePath)
+	if err != nil {
+		return err
+	}
+
+	restaurant.Set("menu", filePathWithChecksum)
+	if err := app.Save(restaurant); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Restaurant) Download(downloadPath string, logger *slog.Logger) (string, error) {
+	logger.Debug("Downloading menu from direct URL", "id", r.ID, "website", r.Website)
+
+	if len(r.Navigate) == 0 || r.Navigate[0].Locator == "" {
+		return "", fmt.Errorf("no URL defined in first locator for restaurant %s", r.ID)
+	}
+
+	url, err := url.Parse(r.Navigate[0].Locator)
+	if err != nil {
+		return "", fmt.Errorf("invalid URL in first locator for restaurant %s: %w", r.ID, err)
+	}
+
+	downloadPath, err = download.Curl(downloadPath, url.String())
+	if err != nil {
+		return "", fmt.Errorf("could not download file %s: %w", url, err)
+	}
+
+	logger.Debug("Successfully downloaded menu", "id", r.ID, "path", downloadPath)
+	return downloadPath, nil
+}
+
+func (r *Restaurant) Scrape(downloadPath string, web *web.Web, logger *slog.Logger) (string, error) {
+	logger.Debug("Scraping restaurant", "id", r.ID, "website", r.Website)
+
+	err := web.Run(r.Website, func(page playwright.Page) error {
+		for i, nav := range r.Navigate {
+			if nav.Style != "" {
+				page.AddStyleTag(playwright.PageAddStyleTagOptions{Content: new(nav.Style)})
+			}
+			nav.Locator = placeholder.Replace(nav.Locator)
+			selector := page.Locator(nav.Locator).First()
+			if i < len(r.Navigate)-1 {
+				logger.Debug("Clicking on locator", "locator", nav.Locator)
+				time.Sleep(time.Duration(rand.Intn(2)+1) * time.Second)
+				if err := selector.Click(); err != nil {
+					return fmt.Errorf("could not click on %s: %w", nav.Locator, err)
+				}
+			} else if r.ContentType != "html" {
+				if nav.Attribute == "" {
+					logger.Debug("Trying to download file by clicking on locator", "locator", nav.Locator)
+					download, err := page.ExpectDownload(func() error {
+						return selector.Click(playwright.LocatorClickOptions{Force: new(true)})
+					})
+					if err != nil {
+						return fmt.Errorf("could not click on %s: %w", nav.Locator, err)
+					}
+					downloadPath = filepath.Join(DownloadsFolder, download.SuggestedFilename())
+					if err := download.SaveAs(downloadPath); err != nil {
+						return fmt.Errorf("could not save download to %s: %w", downloadPath, err)
+					}
+				} else {
+					logger.Debug("Trying to download file by getting attribute", "locator", nav.Locator, "attribute", nav.Attribute)
+					imgSrc, err := selector.GetAttribute(nav.Attribute)
+					if err != nil {
+						return fmt.Errorf("could not get attribute %s: %w", nav.Attribute, err)
+					}
+					downloadPath, err = download.Curl(downloadPath, imgSrc)
+					if err != nil {
+						return fmt.Errorf("could not download file %s: %w", imgSrc, err)
+					}
+				}
+			} else {
+				var err error
+				time.Sleep(2 * time.Second)
+				if nav.Locator != "" {
+					logger.Debug("Making a screenshot of locator", "locator", nav.Locator)
+					locator := page.Locator(nav.Locator).First()
+					locatorCount, err := locator.Count()
+					if err != nil {
+						return fmt.Errorf("could not count locators %s: %w", nav.Locator, err)
+					}
+					if locatorCount == 0 {
+						return fmt.Errorf("no element found for locator %s", nav.Locator)
+					}
+					err = locator.ScrollIntoViewIfNeeded()
+					if err != nil {
+						return fmt.Errorf("could not scroll: %w", err)
+					}
+					_, err = locator.Screenshot(playwright.LocatorScreenshotOptions{
+						Animations: playwright.ScreenshotAnimationsDisabled,
+						Path:       new(downloadPath),
+						Type:       playwright.ScreenshotTypePng,
+					})
+				} else {
+					logger.Debug("Making a screenshot of the full page")
+					_, err = page.Screenshot(playwright.PageScreenshotOptions{
+						Animations: playwright.ScreenshotAnimationsDisabled,
+						Path:       new(downloadPath),
+						FullPage:   new(true),
+						Type:       playwright.ScreenshotTypePng,
+					})
+				}
+				if err != nil {
+					return fmt.Errorf("could not screenshot: %w", err)
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("error scraping restaurant %s: %w", r.ID, err)
+	}
+
+	logger.Debug("Successfully scraped restaurant", "id", r.ID, "path", downloadPath)
+	return downloadPath, nil
+}
